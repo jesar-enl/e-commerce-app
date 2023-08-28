61d1e1cf41c93a4d12afa4da5520e53f7426583f
Jesse Kyambadde <jessekyambadde0@gmail.com>
2023-08-28 11:08:19 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{js, jsx, tsx}', './components/**/*.{js, jsx, tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
